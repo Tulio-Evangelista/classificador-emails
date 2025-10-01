8304d38344b636bcd4634e53f144f93ee2444f92
Tulio Evangelista <136937890+Tulio-Evangelista@users.noreply.github.com>
2025-10-01 16:24:30 -0300

    Update index.py

--- index.py
+++ index.py
@@ -28,7 +28,7 @@ RESPONSES = {
     "Improdutivo": "Obrigado pelo seu contato! Sua mensagem foi recebida."
 }
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/qualquercoisa", methods=["GET", "POST"])
 def index():
     category = None
     response = None
